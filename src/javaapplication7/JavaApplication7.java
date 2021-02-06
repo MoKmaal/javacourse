19da7da721193abd0172911a0ae44e85ad9b1706
Mohamed Kamal <makamal@sumerge.com>
2021-02-06 10:44:23 +0200

    []: My First Code

---
+++ src/javaapplication7/JavaApplication7.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication7;
+
+/**
+ *
+ * @author makamal
+ */
+public class JavaApplication7 {
+
+    public static void main(String[] args) {
+        
+       String c = "I Love Java";
+       
+       c = c.substring(3,9);
+        System.out.println(c);
+    }
+    
+  
+
+}
